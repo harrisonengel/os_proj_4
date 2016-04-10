17580e04957c71547d0475162f4fe2e31353e466
Harrison Engel <hengel@thompson.cs.tcu.edu>
2016-04-10 17:03:44 -0500

    Fixed for FIFO

--- simpager.py
+++ simpager.py
@@ -3,10 +3,11 @@ import pager
 def main():
     refs = map(int, input().split())
     frames = int(input())
-        
+    #print("Page Referene String:\n",' '.join(map(str,refs)))
+    #print("Number of Frames: ", str(frames))
     while(True):
         alg = input()
         fun = pager.getPageAlg(alg)
         faults = fun(refs, frames)
-        print(str(faults))
+        print(alg, ": ", str(faults))
 main()
