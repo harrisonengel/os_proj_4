660143dc271b920813b9491dc25398fea8dd2708
Harrison Engel <hengel1993@gmail.com>
2016-04-10 20:13:22 -0500

    Working but not tested MFU algorithm.

--- mfu.py
+++ mfu.py
@@ -1,5 +1,22 @@
+import sys
 
 def alg(refs, frames):
-    print("MFU: Not Yet Implemented")
-    return 0
+    freq = dict(zip(refs, [0]*len(refs)))
+    q = []
+    faults = 0
+    for i in refs:
+        freq[i] += 1
+        if i not in q:
+            faults += 1
+            if len(q) >= frames:
+                most= 0
+                maxElt = 0
+                for j in q:
+                    if most < freq[j]:
+                        most = freq[j]
+                        maxElt = j
+                q.pop(q.index(maxElt))
+            q.append(i)
+    return faults
+
 
