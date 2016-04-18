4b804f81cca80d747854bffb808147362871574d
Harrison Engel <hengel@thompson.cs.tcu.edu>
2016-04-18 16:46:05 -0500

    Update page test and simpager.

--- simpager.py
+++ simpager.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python3
 import pager
 
 def main():
-    refStr = input() 
-    frames = int(input())
-    print("Page Referene String:\n", refStr)
-    print("Number of Frames: ", str(frames))
-    refs = list(map(int, refStr.split()))
-    while(True):
-        alg = input()
-        fun = pager.getPageAlg(alg)
-        faults = fun(refs, frames)
-        print(alg, ": ", str(faults))
+    try:
+        refStr = input() 
+        frames = int(input())
+        print("Page Referene String:\n", refStr)
+        print("Number of Frames: ", str(frames))
+        refs = list(map(int, refStr.split()))
+        while(True):
+            alg = input()
+            fun = pager.getPageAlg(alg)
+            faults = fun(refs, frames)
+            print(alg, ": ", str(faults))
+    except EOFError:
+        print()
+        
 main()
